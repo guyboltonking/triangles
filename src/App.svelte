e217e3461e5388d9fc02db5a4939f27b78f19fad
Guy Bolton King <guy@waftex.com>
2022-09-21 20:12:38 +0100

    Split out Playfield component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import Playfield from "./Playfield.svelte";
   import { state } from "./triangles.js";
 
-  let lastLoop = 0;
+  let displayWidth = 0;
+  let displayHeight = 0;
+  let lastLoopTimestamp = 0;
   let fps = "";
 
   onMount(() => {
     let frame;
 
     const loop = (timestamp) => {
-      let elapsed = timestamp - lastLoop;
-      lastLoop = timestamp;
+      let elapsed = timestamp - lastLoopTimestamp;
+      lastLoopTimestamp = timestamp;
       if (elapsed != 0) {
         fps = (1000 / elapsed).toFixed();
       }
@@ -22,27 +25,6 @@
 
     return () => cancelAnimationFrame(frame);
   });
-
-  let display;
-  let displayWidth = 0;
-  let displayHeight = 0;
-
-  onMount(() => {
-    displayWidth = display.clientWidth;
-    displayHeight = display.clientHeight;
-
-    const resizeObserver = new ResizeObserver((entries) => {
-      // We don't care about the entries in this callback; we know it's been
-      // updated, just ask the component directly
-      displayWidth = display.clientWidth;
-      displayHeight = display.clientHeight;
-    });
-
-    resizeObserver.observe(display);
-
-    // This callback cleans up the observer
-    return () => resizeObserver.unobserve(display);
-  });
 </script>
 
 <div id="controls">
@@ -50,40 +32,11 @@
   <p>{displayWidth}</p>
   <p>{displayHeight}</p>
 </div>
+
 <!-- TODO: make the viewBox have the same aspect ratio as the svg -->
-<svg
+<Playfield
+  bind:width={displayWidth}
+  bind:height={displayHeight}
   id="display"
-  bind:this={display}
-  viewBox="{$state.viewBox.x} {$state.viewBox.y} {$state.viewBox.width} {$state
-    .viewBox.height}"
->
-  <defs>
-    <pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">
-      <polyline points="0,100 0,0, 100,0" stroke="red" fill="none" />
-    </pattern>
-  </defs>
-  <rect
-    x={$state.viewBox.x}
-    y={$state.viewBox.y}
-    width={$state.viewBox.width}
-    height={$state.viewBox.height}
-    fill="url(#grid)"
-  />
-  {#each $state.positions() as position}
-    <circle cx={position.x} cy={position.y} r="2" />
-  {/each}
-</svg>
-
-<style>
-  #controls {
-    flex: 0 0 auto;
-  }
-
-  #display {
-    border: solid black 1px;
-    /* Don't use auto for the flex-basis: it causes the svg to grow in height to
-    suit the aspect ration of the viewBox, causing a nasty constantly resized
-    svg. */
-    flex: 1 1 0;
-  }
-</style>
+  {state}
+/>
---
+++ src/Playfield.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let state;
+    export let id;
+    export let width;
+    export let height;
+</script>
+
+<div {id} bind:clientWidth={width} bind:clientHeight={height}>
+    <svg
+        viewBox="{$state.viewBox.x} {$state.viewBox.y} {$state.viewBox
+            .width} {$state.viewBox.height}"
+    >
+        <defs>
+            <pattern
+                id="grid"
+                width="100"
+                height="100"
+                patternUnits="userSpaceOnUse"
+            >
+                <polyline points="0,100 0,0, 100,0" stroke="red" fill="none" />
+            </pattern>
+        </defs>
+        <rect
+            x={$state.viewBox.x}
+            y={$state.viewBox.y}
+            width={$state.viewBox.width}
+            height={$state.viewBox.height}
+            fill="url(#grid)"
+        />
+        {#each $state.positions() as position}
+            <circle cx={position.x} cy={position.y} r="2" />
+        {/each}
+    </svg>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-flow: column nowrap;
+    }
+    svg {
+        /* Don't use auto for the flex-basis: it causes the svg to grow in
+        height to suit the aspect ration of the viewBox, causing a nasty
+        constantly resized svg. */
+        flex: 1 1 0;
+        border: solid black 1px;
+    }
+</style>

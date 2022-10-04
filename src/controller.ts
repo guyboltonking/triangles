0dbdb102c783ad8ae8d36aa49d57aa3a20bd7658
Guy Bolton King <guy@waftex.com>
2022-10-04 21:01:51 +0100

    Use classes-for-states for controller

--- src/controller.ts
+++ src/controller.ts
@@ -1,35 +1,90 @@
 import type { Player } from "./model";
 
 export interface ModalController {
-    clickBackground();
-    click(player: Player);
-    mouseOver(player: Player);
-    mouseOut(player: Player);
+    clickBackground(): ModalController;
+    click(player: Player): ModalController;
+    mouseOver(player: Player): ModalController;
+    mouseOut(player: Player): ModalController;
+}
+
+class NullController implements ModalController {
+    clickBackground(): ModalController {
+        return this;
+    }
+    click(player: Player): ModalController {
+        return this;
+    }
+    mouseOver(player: Player): ModalController {
+        return this;
+    }
+    mouseOut(player: Player): ModalController {
+        return this;
+    }
+}
+
+class Editing extends NullController {
+    private static INSTANCE = new Editing();
+    private player: Player;
+
+    static instance(player: Player): ModalController {
+        Editing.INSTANCE.player = player;
+        player.selected.set(true);
+        return Editing.INSTANCE;
+    }
+
+    clickBackground(): ModalController {
+        this.player.selected.set(false);
+        return NoSelection.instance();
+    }
+
+    click(player: Player): ModalController {
+        this.player.selected.set(false);
+        return Editing.instance(player);
+    }
+}
+
+class NoSelection extends NullController {
+    private static INSTANCE = new NoSelection();
+
+    static instance(): ModalController {
+        return NoSelection.INSTANCE;
+    }
+
+    click(player: Player): ModalController {
+        return Editing.instance(player);
+    }
+
+    mouseOver(player: Player): ModalController {
+        player.selected.set(true);
+        return this;
+    }
+
+    mouseOut(player: Player): ModalController {
+        player.selected.set(false);
+        return this;
+    }
 }
 
 export class EditController implements ModalController {
-    private selectedPlayer: Player = null;
+    private controller: ModalController = NoSelection.instance();
 
-    clickBackground() {
-        this.selectedPlayer?.selected.set(false);
-        this.selectedPlayer = null;
+    clickBackground(): ModalController {
+        this.controller = this.controller.clickBackground();
+        return this.controller;
     }
 
-    click(player: Player) {
-        this.selectedPlayer?.selected.set(false);
-        this.selectedPlayer = player;
-        this.selectedPlayer.selected.set(true);
+    click(player: Player): ModalController {
+        this.controller = this.controller.click(player);
+        return this.controller;
     }
 
-    mouseOver(player: Player) {
-        if (this.selectedPlayer == null) {
-            player.selected.set(true);
-        }
+    mouseOver(player: Player): ModalController {
+        this.controller = this.controller.mouseOver(player);
+        return this.controller;
     }
 
-    mouseOut(player: Player) {
-        if (this.selectedPlayer == null) {
-            player.selected.set(false);
-        }
+    mouseOut(player: Player): ModalController {
+        this.controller = this.controller.mouseOut(player);
+        return this.controller;
     }
 }
